34c5607edd0ff3492b04c1d69e7e48b42b619c04
HongSoonBong <hsb310@gmail.com>
2025-04-27 21:28:14 +0900

    Update script.js

--- script.js
+++ script.js
@@ -395,5 +395,29 @@ async function loadFeedbacks() {
 
 // 알레르기 정보 포맷팅
 function formatAllergies(allergies) {
-    return allergies.map(allergy => `<span class="allergy-tag">${allergy}</span>`).join(' ');
+    const allergyMap = {
+        '1': '난류',
+        '2': '우유',
+        '3': '메밀',
+        '4': '땅콩',
+        '5': '대두',
+        '6': '밀',
+        '7': '고등어',
+        '8': '게',
+        '9': '새우',
+        '10': '돼지고기',
+        '11': '복숭아',
+        '12': '토마토',
+        '13': '아황산류',
+        '14': '호두',
+        '15': '닭고기',
+        '16': '쇠고기',
+        '17': '오징어',
+        '18': '조개류'
+    };
+    
+    return allergies.map(code => {
+        const name = allergyMap[code] || code;
+        return `<span class="allergy-tag" data-code="${code}">${name}</span>`;
+    }).join('');
 }
